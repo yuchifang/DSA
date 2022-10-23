7df7188de2eda7aa9cd82ad48ee9aed077b0ef76
fangfelipe <fangfelipe@gmail.com>
2022-10-23 08:10:19 +0800

    update comment

--- DataStructure/LeetCode/HashTable.py/_1TwoSum.py
+++ DataStructure/LeetCode/HashTable.py/_1TwoSum.py
@@ -1,3 +1,9 @@
+'''
+給一個都是數字的 Array, 再給一個數字, 
+回傳陣列裡面任兩個數字相加等於其數字分別的 index
+'''
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dictD = {}
@@ -8,3 +14,11 @@ class Solution:
             else:
                 dictD[target-item] = indexCount
             indexCount += 1
+
+
+'''
+要的是 相減等於其數字 並輸出 index
+邏輯 相減 輸出 index
+相減 A-B = C
+可以用 hashTable 處理這部分
+'''
