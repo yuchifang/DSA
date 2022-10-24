bdcec3eb780bd2cf43fdd36f834f94ec4c3d3a67
fangfelipe <fangfelipe@gmail.com>
2022-10-24 22:07:24 +0800

    update

---
+++ DataStructure/LeetCode/HashTable/_1002FindCommonCharacter.py
@@ -0,0 +1,3 @@
+# https://leetcode.com/problems/find-common-characters/
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
--- test.py
+++ test.py
@@ -3,3 +3,7 @@ from typing import Counter
 
 cnt = Counter([1, 1, 2, 2, 3, 4, 5, 6])
 print(cnt)
+print(cnt[1])
+c = Counter({'red': 4, 'blue': 2})
+c = Counter('gallahad')
+print(c)
