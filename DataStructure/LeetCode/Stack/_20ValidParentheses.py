1758048922449822b20535007b993e95926e5976
danfangortery <Dan.Fang@gmail.com>
2022-10-24 17:45:43 +0800

    update

--- DataStructure/LeetCode/Stack/_20ValidParentheses.py
+++ DataStructure/LeetCode/Stack/_20ValidParentheses.py
@@ -5,19 +5,30 @@
 True
 "()[]{}"
 True
+"){"
+False
 '''
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        for item in range(0,len(s),2):
-            if s[item] == "(" and s[item+1]!= ")":
-                return False
-                
-            if s[item] == "[" and s[item+1]!= "]":
-                return False
-                    
-            if s[item] == "{" and s[item+1]!= "}":
-                return False
-                
-        return True
-        
+        listStack = []
+        newList=[*s]
+        if len(newList) % 2 !=0: return False
+        for item in newList:
+            if len(listStack) == 0:
+               listStack.append(item)
+               continue
+            if item ==")" and listStack[-1] == "(":
+                listStack.pop()
+                continue
+
+            if item =="]"and listStack[-1] == "[":
+                listStack.pop()
+                continue
+
+            if item =="}"and listStack[-1] == "{":
+                listStack.pop()
+                continue
+
+            listStack.append(item)
+        return len(listStack) ==0
