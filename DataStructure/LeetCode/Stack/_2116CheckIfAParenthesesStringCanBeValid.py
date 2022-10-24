ae41a65c8133df66f442d933bfe135890d8791db
danfangortery <Dan.Fang@gmail.com>
2022-10-24 17:48:06 +0800

    update

---
+++ DataStructure/LeetCode/Stack/_2116CheckIfAParenthesesStringCanBeValid.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        
---
+++ DataStructure/LeetCode/Stack/_22GenerateParentheses.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/generate-parentheses/
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
